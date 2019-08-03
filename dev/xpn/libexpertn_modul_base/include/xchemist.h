c4247cff53c76f9ac8ccdcdb0d054d1ddf177e01
ml4ub <41296643+ml4ub@users.noreply.github.com>
2019-08-03 12:07:43 +0200

    Add files via upload

--- dev/xpn/libexpertn_modul_base/include/xchemist.h
+++ dev/xpn/libexpertn_modul_base/include/xchemist.h
@@ -337,7 +337,10 @@ typedef struct cProfile
 	int  dyn_AOM_div; //Switch ot activate new dynamic AOM division by Moritz 
 	int  fSOM1_new; //Switch ot activate new fSOM1 partitioning at BOM death by Moritz     
 	int  dyn_CUE_AOM; //Switch ot activate new dynamic CUE for AOM division by Moritz     
-	double  fStandCropRes_to_AOM2_part_LN; //End of Moritz
+	double  fStandCropRes_to_AOM2_part_LN; 
+    double  L_N_slope; 
+    double  L_N_intercept; 
+    //End of Moritz
 	double  fCStrLitterSurf;
 	double  fNStrLitterSurf;
 	double  fLignFracStrLitSurf;
