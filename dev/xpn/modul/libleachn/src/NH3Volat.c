a1eea00b086731006e95f5979667c70a614d9cd0
Sebastian Gayler <sebastian.gayler@uni-hohenheim.de>
2020-08-25 21:53:51 +0200

    Modified crop parameters for gecros and gecros_h in cfg_template folder; key [gecros_h] used for libgecros_h (xpi, name of crop paramter files, key for crop parameter files)

--- dev/xpn/modul/libleachn/src/NH3Volat.c
+++ dev/xpn/modul/libleachn/src/NH3Volat.c
@@ -49,6 +49,33 @@ int leachn_NH3Volat1_load(stickstoff *self)
 			PRINT_ERROR("Problem with hydraulic functions!");
 		}*/
     
+    expertn_modul_base *xpn = &(self->parent);
+	PCHEMISTRY pCh = xpn->pCh;
+	double NH3VolatMaxR;
+//	PTIME pTi = xpn->pTi;
+	
+	// in Expert-N 3.0 xnm File Marker: 20013
+	//Max. NH3-N Volatilisation | NH4-N Deposition | NO3-N Deposition
+	// [1/d]            |   [kg N /ha /a]  |   [kg N /ha /a] 
+//	----------------------------------------------------------------------
+//20013
+// 5.e-002				12.				6.				
+
+	// kg N /ha / a
+	NH3VolatMaxR = xpn_register_var_get_pointer_convert_to_double(self->parent.pXSys->var_list,"Config.volatilization.maxNH3Volat",0.1);
+
+/*	// kg N /ha / a
+	fNO3NImisRa = xpn_register_var_get_pointer_convert_to_double(self->parent.pXSys->var_list,"Config.Constant Deposition.no3",0.0);
+
+	// kg N /ha / a -> kg N /ha / d
+	pCh->pCProfile->fNH4NImisR = fNH4NImisRa / 365.25;
+	
+	// kg N /ha / a -> kg N /ha / d
+	pCh->pCProfile->fNO3NImisR = fNO3NImisRa / 365.25;*/
+
+	pCh->pCProfile->fNH3VolatMaxR = NH3VolatMaxR;
+
+    
     return RET_SUCCESS;
 }
 
